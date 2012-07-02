b19aa6c1cb02d33cd4c29ffd3f122846c8540aa3
sminolli <sminolli@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2012-07-02 09:07:06 +0000

    Added Doxygenized headers.
    git-svn-id: svn+ssh://svn.code.sf.net/p/tango-cs/code/share/yat/trunk@20811 4e9c00fd-8f2e-0410-aa12-93ce3db5e235

--- include/yat/utils/Callback.h
+++ include/yat/utils/Callback.h
@@ -43,51 +43,62 @@ namespace yat
 {
 
 // ============================================================================
-// class: CallbackContainer (abstract)
+//! \class CallbackContainer 
+//! \brief Generic callback container class.
+//!
+//! This is the base class for generic callback management.
+//! This abstract class can not be used as this and must be derived.
 // ============================================================================
 template <typename P>
 class CallbackContainer
 {
 public:
-  //! destructor
+  //! \brief Destructor.
   virtual ~CallbackContainer()
   {};
 
-  //! do/exec/... operator with T as argument
+  //! \brief Executes operation with \<P\> as argument.
+  //! \param p Template type \<P\> argument.
   virtual void operator() (P) const = 0;
 
-  //! deep copy
+  //! \brief Deep copy.
   virtual CallbackContainer<P>* clone () const = 0;
 
-  //! comparation
+  //! \brief Comparison operator with another CallbackContainer\<P\> object.
   virtual bool is_equal (const CallbackContainer<P> *) const = 0;
 };
 
 // ============================================================================
-// class: Callback
+//! \class Callback 
+//! \brief Generic callback class.
+//!
+//! This template class has a CallbackContainer member.
 // ============================================================================
 template <typename P> 
 class Callback
 {
 public:
-  //! constructor
+  //! \brief Constructor from a CallbackContainer object.
+  //! \param c CallbackContainer object.
   Callback (CallbackContainer<P>* c)
     : container(c)
   {};
 
-  //! constructor
+  //! \brief Copy constructor.
+  //! \param c The source object.
   Callback (const Callback<P>& c)
     : container(c.container ? c.container->clone() : 0)
   {};
 
-  //! destructor
+  //! \brief Destructor.
   ~Callback ()
   {
     delete this->container;
     this->container = 0;
   };
 
-  //! assignment operator
+  //! \brief operator=.
+  //! \param callback The source object.
   Callback<P>& operator= (const Callback<P>& callback)
   {
     if (this != &callback)
@@ -98,7 +109,9 @@ public:
     return *this;
   };
 
-  //! do operation defined in body
+  //! \brief Executes operation defined in body.
+  //! \param p Generic type \<P\> argument.
+  //! \exception NULL_POINTER Thrown when the callback is null.
   void operator() (P p) 
   {
     if (this->container)
@@ -107,13 +120,18 @@ public:
       THROW_YAT_ERROR("NULL_POINTER", "null/empty callback called", "Callback::operator()");
   }
   
-  //! empty callback?  
+  //! \brief Is the callback empty?  
+  //!
+  //! Returns true if the callback is null, false otherwise.
   bool is_empty () const
   {
     return this->container == 0;
   };
 
-  //! comparation
+  //! \brief operator==.
+  //!
+  //! Compares the CallbackContainer\<P\> objects.
+  //! \param s The source object.
   bool operator== (const Callback<P>& s) const
   {
     if (this->container == 0 && s.container == 0)
@@ -130,31 +148,76 @@ private:
 };
 
 // ============================================================================
-// class: FunctionCallbackContainer (CallbackContainer for 'free' functions)
+//! \class FunctionCallbackContainer 
+//! \brief Generic callback class for a 'free' function.
+//!
+//! This template class provide an interface for a generic callback that uses a
+//! specified 'free' function.
+//! Inherits from CallbackContainer class.
+//!
+//! The 'free' function is defined by:
+//! - a generic type \<P\> for the 'function argument type,
+//! - a generic type \<F\> for the function type.
+//! 
+//! \b USAGE:
+//! - %Callback definition:\n
+//! For the callback definition, use the YAT MACRO, which defines the three types of 
+//! callbacks derived from FunctionCallbackContainer, MemberCallbackContainer and Callback 
+//! classes.\n
+//! This MACRO also defines *instanciate()* functions providing a way to connect the callback to 
+//! a 'free' function or a member function of an object.\n
+//! For instance:
+//! \verbatim YAT_DEFINE_CALLBACK(myCallbackType, myCallbackDataType &); \endverbatim
+//! The previous line defines the following callback prototype: 
+//! \verbatim myCallbackType CBk; \endverbatim
+//! that will refer to the following function prototype:
+//! \verbatim void myFunction(myCallbackDataType& param) \endverbatim
+//! The callback data type must alos be defined. For instance:
+//! \verbatim struct myCallbackDataType { ... }; \endverbatim
+//!
+//! - %Callback instantiation: \n
+//! Example for a 'free' function:
+//! \verbatim CBk = yat::myCallbackType::instanciate(&myFunction); \endverbatim
+//! where myFunction has the following prototype:
+//! \verbatim void myFunction(myCallbackDataType& param) \endverbatim
+//! Example for a member function:
+//! \verbatim CBk = yat::myCallbackType::instanciate(*this, &myClass::myFunction); \endverbatim
+//! where myFunction has the following prototype:
+//! \verbatim void myClass::myFunction(myCallbackDataType& param) \endverbatim
+//!
 // ============================================================================
 template <typename P, class F>
 class FunctionCallbackContainer : public CallbackContainer<P>
 {
 public:
-  //! constructor taking a function pointer
+  //! \brief Constructor taking a function pointer.
+  //! \param function Pointer on callback function (generic type \<F\>).
   FunctionCallbackContainer (const F & function)
     : function_(function)
   {}
 
-  //! execute operation: call the function
+  //! \brief Executes operation: calls the function.
+  //! \param p Generic type \<P\> argument of the function.
   void operator() (P p) const 
   { 
     if (function_) 
       function_(p);
   }
 
-  //! comparation
+  //! \brief Comparison with another FunctionCallbackContainer object.
+  //!
+  //! Compares the pointers to callback functions.
+  //! \param s The source object.
   bool _is_equal (const FunctionCallbackContainer* s) const
   {
     return this->function_ == s->function_;
   }
 
-  //! comparation
+  //! \brief Comparison with a CallbackContainer object.
+  //!
+  //! Compares the callback functions if \<s\> type is compatible
+  //! with FunctionCallbackContainer.
+  //! \param s The source object.
   bool is_equal (const yat::CallbackContainer<P>* s) const
   {
     const FunctionCallbackContainer* fcc = dynamic_cast<const FunctionCallbackContainer*>(s);
@@ -163,37 +226,79 @@ public:
   }
 
 private:
-  //! the callback function
+  //- the callback function
   const F function_;
 
-  //! no assignemt operator
+  //- no assignment operator
   FunctionCallbackContainer& operator=(FunctionCallbackContainer&);
 };
 
 // ============================================================================
-// class: MemberCallbackContainer (CallbackContainer for 'member' functions)
+//! \class MemberCallbackContainer 
+//! \brief Generic callback class for 'member' functions.
+//!
+//! This template class provide a callback interface that uses a member function.
+//! Inherits from CallbackContainer class. \n
+//! 
+//! The class and member function are defined by:
+//! - a generic type \<P\> for the member function argument type,
+//! - a generic type \<C\> for the class type,
+//! - a generic type \<M\> for the member function type.\n
+//! 
+//! \b USAGE:
+//! - %Callback definition:\n
+//! For the callback definition, use the YAT MACRO, which defines the three types of 
+//! callbacks derived from FunctionCallbackContainer, MemberCallbackContainer and Callback 
+//! classes. \n 
+//! This MACRO also defines *instanciate()* functions providing a way to connect the callback to 
+//! a 'free' function or a member function of an object.\n
+//! For instance:
+//! \verbatim YAT_DEFINE_CALLBACK(myCallbackType, myCallbackDataType &); \endverbatim
+//! The previous line defines the following callback prototype: 
+//! \verbatim myCallbackType CBk; \endverbatim
+//! that will refer to the following function prototype:
+//! \verbatim void myFunction(myCallbackDataType& param) \endverbatim
+//! The callback data type must alos be defined. For instance:
+//! \verbatim struct myCallbackDataType { ... }; \endverbatim
+//!
+//! - %Callback instantiation:\n
+//! Example for a 'free' function:
+//! \verbatim CBk = yat::myCallbackType::instanciate(&myFunction); \endverbatim
+//! where myFunction has the following prototype:
+//! \verbatim void myFunction(myCallbackDataType& param) \endverbatim
+//! Example for a member function:
+//! \verbatim CBk = yat::myCallbackType::instanciate(*this, &myClass::myFunction); \endverbatim
+//! where myFunction has the following prototype:
+//! \verbatim void myClass::myFunction(myCallbackDataType& param) \endverbatim
+//!
 // ============================================================================
 template <typename P, class C, class M>
 class MemberCallbackContainer : public CallbackContainer<P>
 {
 public:
-  //! Member function type
+  //! \brief Member function type.
   typedef void (C::*PMEMBERFUNC)(P);
 
 
-  //! constructor taking a function pointer
+  //! \brief Constructor taking a function pointer.
+  //! \param client The class.
+  //! \param member The function which belongs to the class.
   MemberCallbackContainer (C& client, M member)
     : client_(client), member_(member)
   {}
 
-  //! execute operation: call the function
+  //! \brief Executes operation: calls the function.
+  //! \param p Generic type \<P\> argument of the class function.
   void operator() (P p) const 
   { 
     if (member_) 
       (client_.*member_)(p);
   }
 
-  //! comparation
+  //! \brief Comparison with another MemberCallbackContainer object.
+  //! 
+  //! Compares member functions.
+  //! \param s The source object.
   bool _is_equal (const MemberCallbackContainer* s) const
   {
     if (&this->client_ != &s->client_)
@@ -202,7 +307,11 @@ public:
     return member_ == s->member_;
   }
 
-  //! comparation
+  //! \brief Comparison with a CallbackContainer object.
+  //!
+  //! Compares the member functions if \<s\> type is compatible
+  //! with MemberCallbackContainer.
+  //! \param s The source object.
   bool is_equal (const yat::CallbackContainer<P>* s) const
   {
     const MemberCallbackContainer* mcc = dynamic_cast<const MemberCallbackContainer*>(s);
@@ -211,18 +320,18 @@ public:
   }
 
 private:
-  //! The object the method function belongs to
+  //- The object the method function belongs to.
   C& client_;
 
-  //! The method to call
+  //- The method to call.
   PMEMBERFUNC member_;
 
-  //! no assignement operator
+  //- no assignment operator.
   MemberCallbackContainer& operator=(MemberCallbackContainer&);
 };
 
 // ============================================================================
-// YAT_DEFINE_CALLBACK: the magical macro 
+//! YAT_DEFINE_CALLBACK: the magical macro.
 // ============================================================================
 #define YAT_DEFINE_CALLBACK(CallbackClass, CallbackArg) \
   template <typename Function> \
