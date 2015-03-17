ce2061a6adeca43c6aeb97c0aac51146658a09de
nleclercq <nleclercq@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2015-03-17 13:45:05 +0000

    Copyright (c) 2004-2015 Synchrotron SOLEIL
    git-svn-id: svn+ssh://svn.code.sf.net/p/tango-cs/code/share/yat/trunk@27672 4e9c00fd-8f2e-0410-aa12-93ce3db5e235

--- src/plugin/PlugInUnix.cpp
+++ src/plugin/PlugInUnix.cpp
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------
-// Copyright (c) 2004-2014 Synchrotron SOLEIL
+// Copyright (c) 2004-2015 Synchrotron SOLEIL
 // All rights reserved. This program and the accompanying materials
 // are made available under the terms of the GNU Lesser Public License v3
 // which accompanies this distribution, and is available at
